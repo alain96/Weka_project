cc053effd1ba7691ba0d7afd480ad15efeaba9ee
Alain Blanco <alain.b.1996@gmail.com>
2019-04-01 17:19:10 +0200

    Eraiki modeloa aldatuta

--- weka proiektua/src/kodea/EraikiModeloa.java
+++ weka proiektua/src/kodea/EraikiModeloa.java
@@ -4,16 +4,31 @@ import java.util.Vector;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.functions.LinearRegression;
 import weka.core.SerializationHelper;
 
 public class EraikiModeloa {
 	
 	public static void main(String[] args) {
-		Evaluation eval = args[0]; 
-		Classifier cls = args[1];
+		Evaluation eval;
+		LinearRegression LR = new LinearRegression();
+		LR.buildClassifier(train);
+//		IBk	cls = new IBk(kOpt);
+//      cls.buildClassifier(train);
+//      etiketa = new SelectedTag(wOpt,etiketak);
+//		linearnn = new LinearNNSearch();
+//    	linearnn.setDistanceFunction(df[dOpt]);
+//      cls.setNearestNeighbourSearchAlgorithm(linearnn);
+//		cls.setDistanceWeighting(etiketa);
+		
+        eval = new Evaluation(train);
+        eval.crossValidateModel(LR, test, 10, new Random(1));
+		
+		
+		
 		Vector v = new Vector();
         v.add(eval);
-        v.add(cls);
+        v.add(LR);
         System.out.println(eval);
         System.out.println(cls);
         SerializationHelper.write("Modeloa.model", v);
